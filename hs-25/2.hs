317ff86f06a4a90a42d8798567475f2ea4af4ec8
chxry <floppa9@proton.me>
2025-12-02 21:29:14 +0000

    lints

--- hs-25/2.hs
+++ hs-25/2.hs
@@ -8,16 +8,16 @@ invalidn :: String -> Int -> Bool
 invalidn xs n | mod (length xs) n == 0 = alleq (chunksOf n xs)
               | otherwise = False
 
-alleq (x:xs) = and (map (==x) xs)
+alleq (x:xs) = all (==x) xs
 
 invalid2 :: String -> Bool
-invalid2 xs = or (map (invalidn xs) [1..length xs - 1])
+invalid2 xs = any (invalidn xs) [1..length xs - 1]
 
 range :: String -> [Int]
-range x = let (a:b:[]) = splitOn "-" x
+range x = let [a, b] = splitOn "-" x
           in [read a..read b]
 
-sol f s = sum (filter (\x -> f (show x)) (concat (map range (splitOn "," s))))
+sol f s = sum (filter (f . show) (concatMap range (splitOn "," s)))
 
 sol1 = sol invalid1
 sol2 = sol invalid2
