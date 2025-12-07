5eb6f188613b244667c6c2876f58765ac7247d8a
chxry <floppa9@proton.me>
2025-12-07 05:47:24 +0000

    fix that at least

--- hs-25/7.hs
+++ hs-25/7.hs
@@ -1,11 +1,13 @@
 import Data.List
 import Data.Maybe
 
-f xs s = nub $ concat [if s !! x == '.' then [x] else [x - 1, x + 1] | x <- xs]
+f (xs, n) s =
+  let (xss, ns) = unzip [if s !! x == '^' then ([x - 1, x + 1], 1) else ([x], 0) | x <- xs]
+  in (nub $ concat xss, n + sum ns)
 
-merge = map (\xs -> (fst (head xs), sum (map snd xs))) . groupBy (\a b -> fst a == fst b)
+merge = map (\xs -> (fst $ head xs, sum $ map snd xs)) . groupBy (\a b -> fst a == fst b)
 
-f2 xs s = merge $ concat [if s !! x == '.' then [(x, n)] else [(x - 1, n), (x + 1, n)] | (x, n) <- xs]
+f2 xs s = merge $ concat [if s !! x == '^' then [(x - 1, n), (x + 1, n)] else [(x, n)] | (x, n) <- xs]
 
 parse s =
   let (l:ls) = lines s
@@ -13,7 +15,7 @@ parse s =
 
 sol1 s =
   let (x, ls) = parse s
-  in sum $ zipWith (\l xs -> length $ filter (\x -> l !! x /= '.') xs) ls (scanl f [x] ls)
+  in snd $ foldl f ([x], 0) ls
 
 sol2 s =
   let (x, ls) = parse s
